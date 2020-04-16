0a4c606a4e4f490cd38ccff27d2a246e891b1e5b
Dilshodbeka <abdullayev.dima7080@gmail.com>
2020-04-16 11:19:01 +0200

    first time adding components

---
+++ src/components/PostForm.js
@@ -0,0 +1,18 @@
+import  React from 'react'
+import { render } from 'react-dom'
+
+export default class PostForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Post Form</h1>
+      </div>
+    )
+  }
+
+}
